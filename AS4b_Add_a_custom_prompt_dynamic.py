3a0912f4f35ffe8e2ea49f414b1bc19b34bffaea
Jose M. Albarrán <jmalbarran@b-thinking.com>
2025-07-25 17:52:28 +0200

    AS4 Prompts with langchain-oracle

--- AS4b_Add_a_custom_prompt_dynamic.py
+++ AS4b_Add_a_custom_prompt_dynamic.py
@@ -2,7 +2,8 @@ import os
 import logging
 from dotenv import load_dotenv
 
-from langchain_community.chat_models.oci_generative_ai import ChatOCIGenAI
+# from langchain_community.chat_models.oci_generative_ai import ChatOCIGenAI
+from langchain_oci.chat_models.oci_generative_ai import ChatOCIGenAI
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, AnyMessage
 from langchain_core.runnables import RunnableConfig
 from langchain_core.prompts import ChatPromptTemplate
